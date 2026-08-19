31a4ccf3d44227c0136cda3b38b4dc8c82b78997
dapursahabat <dapursahabat26@gmail.com>
2026-08-19 18:02:02 +0700

    update

--- script.js
+++ script.js
@@ -5,15 +5,3 @@ const currentYear = document.getElementById("current-year");
 if (currentYear) {
   currentYear.textContent = new Date().getFullYear();
 }
-
-const placeholderLinks = document.querySelectorAll(
-  'a[href^="GANTI_"], a[href*="GANTI-DOMAIN"]'
-);
-
-placeholderLinks.forEach((link) => {
-  link.setAttribute("aria-disabled", "true");
-
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-  });
-});
